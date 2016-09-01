3af9418b0ee08d918cfcf1b0c2e33c7773b34b4a
Micha Zana <micha@yotpo.com>
2016-09-02 00:07:10 +0300

    mission completed c' '

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,33 @@
 class WelcomeController < ApplicationController
+
+  require 'csv'
+
   def index
-    document = Nokogiri::HTML('http://www.google.com')
-    @result = 'micha'
+
+    CSV.open("../../../Desktop/totto-16.csv", "wb") do |csv|
+      csv << ["round #", "1", "x", "2"]
+      @result = {}
+      response = HTTParty.get('http://ibet.co.il/Toto16ResultsArchive.php')
+      document = Nokogiri::HTML(response)
+
+      rounds_numbers = []
+      document.css('select#roundNum option').each do |round|
+        round_number = round.attribute('value').value.to_i
+        rounds_numbers << round_number if round_number != 0
+      end
+
+      rounds_numbers.each do |round_number|
+        puts "round number #{round_number}"
+        @result[round_number] = {}
+        response = HTTParty.get("http://ibet.co.il/Toto16ResultsArchive.php?roundNum=#{round_number}")
+        document = Nokogiri::HTML(response)
+
+        document.css('tr td:first-child').each do |game|
+          game_result = game.text
+          @result[round_number][game_result] = (@result[round_number][game_result] || 0) + 1
+        end
+        csv << [round_number, @result[round_number]['1'], @result[round_number]['x'] || @result[round_number]['X'] || 0, @result[round_number]['2']]
+      end
+    end
   end
 end
