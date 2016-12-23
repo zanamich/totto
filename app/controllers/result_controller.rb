1448d6c8882f7da55c61255100934f17198e3c33
Micha Zana <zanamich@Michas-MacBook-Pro.local>
2016-12-23 22:48:59 +0200

    resque events

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -4,7 +4,18 @@ class ResultController < ApplicationController
 
   def index
 
-    Resque.enqueue(FormGenerator)
+    Resque.enqueue(FormGenerator, :create_permutations, { 'form_structure' => [4,7,5],
+                                                          'bankers' => { 0 => '1',
+                                                                         3 => 'X',
+                                                                         4 => '1',
+                                                                         7 => '2',
+                                                                         8 => '2',
+                                                                         10 => 'X',
+                                                                         13 => '1'
+                                                                        },
+                                                          'index' => 0,
+                                                          'form' => %w(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0)
+                                                        })
 
     render 'result/index'
   end
--- app/jobs/form_generator.rb
+++ app/jobs/form_generator.rb
@@ -5,75 +5,65 @@ class FormGenerator
   @queue = :FormGenerator
   @result_forms = []
 
-  def self.perform
-    @bankers = { 0 => '1',
-                 3 => 'X',
-                 4 => '1',
-                 7 => '2',
-                 8 => '2',
-                 10 => 'X',
-                 13 => '1'
-    }
-    form_structure = create_history_csv
-    create_permutations form_structure, [], 0, %w(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0)
+  EVENTS = [:create_history_csv, :create_permutations].freeze
+
+  def self.perform(event, context)
+    FormGenerator.public_send(event, context) if EVENTS.include?(event.to_sym)
     puts 'done'
   end
 
-  def self.create_history_csv
-    # CSV.open("../../../Desktop/totto-16.csv", "wb") do |csv|
-    #   csv << ["round #", "1", "x", "2"]
-    #   @result = {}
-    #   response = HTTParty.get('http://ibet.co.il/Toto16ResultsArchive.php')
-    #   document = Nokogiri::HTML(response)
-    #
-    #   rounds_numbers = []
-    #   document.css('select#roundNum option').each do |round|
-    #     round_number = round.attribute('value').value.to_i
-    #     rounds_numbers << round_number if round_number != 0
-    #   end
-    #
-    #   total_rounds = rounds_numbers.count
-    #   counter = 1
-    #   rounds_numbers.each do |round_number|
-    #     puts "round number #{round_number}, total #{counter}/#{total_rounds}"
-    #     @result[round_number] = {}
-    #     response = HTTParty.get("http://ibet.co.il/Toto16ResultsArchive.php?roundNum=#{round_number}")
-    #     document = Nokogiri::HTML(response)
-    #
-    #     document.css('tr td:first-child').each do |game|
-    #       game_result = game.text
-    #       @result[round_number][game_result] = (@result[round_number][game_result] || 0) + 1
-    #     end
-    #     if (@result[round_number]['1'] || 0) + (@result[round_number]['x'] || @result[round_number]['X'] || 0) + (@result[round_number]['2'] || 0) >= 15
-    #       csv << [round_number, @result[round_number]['1'] || 0, @result[round_number]['x'] || @result[round_number]['X'] || 0, @result[round_number]['2'] || 0]
-    #     end
-    #     counter += 1
-    #   end
-    # end
+  def self.create_history_csv(context)
+    CSV.open("../../../Desktop/totto-16.csv", "wb") do |csv|
+      csv << ["round #", "1", "x", "2"]
+      @result = {}
+      response = HTTParty.get('http://ibet.co.il/Toto16ResultsArchive.php')
+      document = Nokogiri::HTML(response)
 
-    return [4,7,5]
+      rounds_numbers = []
+      document.css('select#roundNum option').each do |round|
+        round_number = round.attribute('value').value.to_i
+        rounds_numbers << round_number if round_number != 0
+      end
+
+      total_rounds = rounds_numbers.count
+      counter = 1
+      rounds_numbers.each do |round_number|
+        puts "round number #{round_number}, total #{counter}/#{total_rounds}"
+        @result[round_number] = {}
+        response = HTTParty.get("http://ibet.co.il/Toto16ResultsArchive.php?roundNum=#{round_number}")
+        document = Nokogiri::HTML(response)
+
+        document.css('tr td:first-child').each do |game|
+          game_result = game.text
+          @result[round_number][game_result] = (@result[round_number][game_result] || 0) + 1
+        end
+        if (@result[round_number]['1'] || 0) + (@result[round_number]['x'] || @result[round_number]['X'] || 0) + (@result[round_number]['2'] || 0) >= 15
+          csv << [round_number, @result[round_number]['1'] || 0, @result[round_number]['x'] || @result[round_number]['X'] || 0, @result[round_number]['2'] || 0]
+        end
+        counter += 1
+      end
+    end
+  end
+
+  def self.create_permutations(context)
+
+    return unless validate_instances(context['form'].dup, context['form_structure'], context['bankers'])
+
+    context['form'][context['index']] = '1'
+    puts 'A: ' + context['form'].inspect
+    create_permutations({ 'form_structure' => context['form_structure'], 'bankers' => context['bankers'], 'index' => context['index'] + 1, 'form' => context['form'].dup }) if context['index'] <= 15
+
+    context['form'][context['index']] = 'X'
+    puts 'B: ' +  context['form'].inspect
+    create_permutations({ 'form_structure' => context['form_structure'], 'bankers' => context['bankers'], 'index' => context['index'] + 1, 'form' => context['form'].dup }) if context['index'] <= 15
+
+    context['form'][context['index']] = '2'
+    puts 'C: ' +  context['form'].inspect
+    create_permutations({ 'form_structure' => context['form_structure'], 'bankers' => context['bankers'], 'index' => context['index'] + 1, 'form' => context['form'].dup }) if context['index'] <= 15
 
   end
 
-  def self.create_permutations form_structure, bankers, index, form
-
-    return unless validate_instances(form, form_structure)
-
-    form[index] = '1'
-    puts 'A: ' + form.inspect
-    create_permutations form_structure, bankers, index + 1, form.dup if index < 15
-
-    form[index] = 'X'
-    puts 'B: ' +  form.inspect
-    create_permutations form_structure, bankers, index + 1, form.dup if index < 15
-
-    form[index] = '2'
-    puts 'C: ' +  form.inspect
-    create_permutations form_structure, bankers, index + 1, form.dup if index < 15
-
-  end
-
-  def self.validate_instances form, form_structure
+  def self.validate_instances form, form_structure, bankers
     counts = Hash.new 0
     num_of_guesses = 0
 
@@ -89,8 +79,8 @@ class FormGenerator
 
     return false if num_of_guesses > 16
 
-    @bankers.each do |index, value|
-      if form[index] != value && form[index] != '0'
+    bankers.each do |index, value|
+      if form[index.to_i] != value && form[index.to_i] != '0'
         puts 'BANKER CONFLICT'
         return false
       end
